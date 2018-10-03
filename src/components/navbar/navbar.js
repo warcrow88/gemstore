df1c3754752d4c0edcb58b5d381429d0c0c50a90
Todd Paulsen <toddpaulsen@gmail.com>
2018-10-03 12:20:54 -0400

    navlinks working properly TODO render proper components in views.

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './navbar.css';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
 
@@ -7,7 +8,7 @@ class Navbar extends Component {
     return (
       <div className="Navbar">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <NavLink className="navbar-brand" to='/'>Gemstore</NavLink>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -15,24 +16,14 @@ class Navbar extends Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <NavLink className="nav-link" to='/'>Home</NavLink>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
+                  <NavLink className="nav-link" to='/checkout'>Checkout</NavLink>
                 </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
+
                 <li className="nav-item">
-                  <a className="nav-link disabled" href="#">Disabled</a>
+                  <NavLink className="nav-link disabled" to='/checkout'>Total: $0.00</NavLink>
                 </li>
               </ul>
 
