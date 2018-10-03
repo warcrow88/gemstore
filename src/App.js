0df4a8632be0ce89b3a5517fec49e99094b618f0
Todd Paulsen <toddpaulsen@gmail.com>
2018-10-03 10:59:35 -0400

    Included app components, set up product file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import products from './static/data/products.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: undefined,
+      cart: undefined,
+      products: undefined
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      total: 0,
+      cart: [],
+      products: products
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        
+
 
       </div>
     );
